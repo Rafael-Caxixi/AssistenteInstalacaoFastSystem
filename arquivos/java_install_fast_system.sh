bab80bf6257e3e93a4ea2d92892ca89c1b3bc97f
Fuzeti <rfuzeti@deloitte.com>
2022-11-23 17:31:20 -0300

    a

--- arquivos/java_install_fast_system.sh
+++ arquivos/java_install_fast_system.sh
@@ -51,7 +51,7 @@ fi
 
 #----------------BAIXANDO O DOCKER -----------------#
 
-docker –version
+docker --version
 if [ $? -eq 0 ]
     then
 		echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) : Olá você já tem o docker instalado!!"
@@ -60,8 +60,8 @@ if [ $? -eq 0 ]
 		echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Opa! Não identifiquei nenhuma versão do docker instalado, mas sem problemas, irei resolver isso agora!"
 		sleep 2
 		echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Confirme para mim se realmente deseja instalar o Docker (Y/n)?"		
-	read inst
-    if [ \"$inst\" == \"Y\" ]
+	read inste
+    if [ \"$inste\" == \"Y\" ]
 		then
 			echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Ok! Você escolheu instalar o docker ;D"
 			sleep 2
