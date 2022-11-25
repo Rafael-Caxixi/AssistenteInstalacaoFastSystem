8fbbafcbc26693889dd3d680799f3c8408e798b8
Fuzeti <rfuzeti@deloitte.com>
2022-11-24 21:05:08 -0300

    a

--- arquivos/java_install_fast_system.sh
+++ arquivos/java_install_fast_system.sh
@@ -53,7 +53,7 @@ fi
 #----------------BAIXANDO O DOCKER -----------------#
 	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) : LINHA 54"
 docker --version
-	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) : LINHA 55"
+	
 	if [ $? -eq 0 ]
 	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) : LINHA 58"
     then
@@ -68,7 +68,9 @@ docker --version
 			echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) : LINHA 69"
 			sleep 2
 			sudo docker exec -it FastSystem bash
+			echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) : LINHA 71"
 			sleep 2
+			echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) : LINHA 73"
 			mysql -u root -p
 			urubu100
 			CREATE DATABASE FastSystem;
